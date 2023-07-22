1bf76c85ae352443be97e88dbd6d443a09072471
K.M <indiangirl.0914@gmail.com>
2023-07-22 09:16:17 +0900

    引数名のタイプミスを修正

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -4,8 +4,8 @@ import { View, StyleSheet } from 'react-native';
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 
-export default function MemoListScreen(proprs) {
-  const { navigation } = proprs;
+export default function MemoListScreen(props) {
+  const { navigation } = props;
   return (
     <View style={styles.container}>
       <MemoList />
